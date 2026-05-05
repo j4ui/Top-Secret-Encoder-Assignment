224ba4dd2556ef4a8c6ed8d2f82d4c7fc9b1b77f
Aaron Avdeev <underio3298@gmail.com>
2026-05-05 13:44:02 -0400

    most of the assignment is done, just need to add error checks, and compartmentalize as much as possible

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter an unencrypted word; ");
-        String unencryptedWord = scanner.nextLine();
-        System.out.println("Enter an encryption key; ");
-        String encryptionKey = scanner.nextLine();
-        int shiftIndex = determineShiftIndex(encryptionKey);
-        System.out.println(shiftIndex);
-        System.out.println(encryptWord(unencryptedWord, shiftIndex));
+        encryptionDecryptionMenu();
     }
+
+    public static void encryptionDecryptionMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("Top Secret Word Encrypt / Decrypt\nEnter an option:\n(1) Encrypt\n(2) Decrypt\n(0) Exit\nOption: ");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1: encryptWord(); break;
+                case 2: decryptWord(); break;
+                case 0: return;
+            }
+        } while (option != 0);
+    }
+
     public static int determineShiftIndex(String key) {
         int shiftIndex = 0;
         for (int i = 0; i < key.length(); i++) {
@@ -19,7 +27,13 @@ public class Main {
         return shiftIndex;
     }
 
-    public static String encryptWord(String word, int shiftIndex) {
+    public static void encryptWord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an unencrypted word: ");
+        String word = scanner.nextLine();
+        System.out.println("Enter an encryption key");
+        String key = scanner.nextLine();
+        int shiftIndex = determineShiftIndex(key);
         String encryptedWord = "";
         for (int i = 0; i < word.length(); i++) {
             int charIndex = word.charAt(i) - 96;
@@ -29,6 +43,27 @@ public class Main {
             }
             encryptedWord += (char) (newCharIndex + 96);
         }
-        return encryptedWord;
+        System.out.println("\nEncoded word: " + encryptedWord);
+
+    }
+
+    public static void decryptWord() {
+        Scanner scanner = new Scanner(System.in);
+        String word, key;
+        String decryptedWord = "";
+        System.out.println("Enter an encrypted word: ");
+        word = scanner.nextLine();
+        System.out.println("Enter an encryption key: ");
+        key = scanner.nextLine();
+        int shiftIndex = determineShiftIndex(key);
+        for (int i = 0; i < word.length(); i++) {
+            int charIndex = word.charAt(i) - 96;
+            int newCharIndex = charIndex - shiftIndex;
+            if (newCharIndex < 1) {
+                newCharIndex = newCharIndex + 26;
+            }
+            decryptedWord += (char) (newCharIndex + 96);
+        }
+        System.out.println("\nDecoded word: " + decryptedWord);
     }
 }
